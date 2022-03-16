c1683f9590da0fa9cd91f4fc450b81cebd45c6df
luxueqing <xq.lu@tron.network>
2022-03-16 18:35:32 +0800

    update build shell script

--- images-build.sh
+++ images-build.sh
@@ -1,4 +1,6 @@
-# Re-use our github folder
+#---------------------bttc---------------------
+BTTC_VERSION=$1
+# use our github folder
 mkdir -p ~/github
 cd ~/github
 
@@ -7,24 +9,24 @@ git clone https://github.com/bttcprotocol/bttc.git
 cd bttc
 
 # This is the current recommended version
-git checkout v1.0.1
+git checkout $BTTC_VERSION
 
 # Build the docker image
-docker build --tag bttc:v1.0.1 .
+docker build --tag bttc:$BTTC_VERSION .
 
 #---------------------delivery---------------------
-
+DELIVERY_VERSION=$2
 cd ~/github
 # Clone the repo
 git clone https://github.com/bttcprotocol/delivery.git
 cd delivery
 
 # Checkout the mainnet release version
-git checkout v1.0.0
+git checkout $DELIVERY_VERSION
 
 # Copy the Dockerfile up a directory
 cp docker/Dockerfile .
 
 # Build the docker image
-docker build --tag delivery:v1.0.0 .
+docker build --tag delivery:$DELIVERY_VERSION .
 
